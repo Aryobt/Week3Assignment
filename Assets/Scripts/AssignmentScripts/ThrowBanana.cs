242f9f488844402867748ce13ac31245b4c5d9be
Aryobt <aryobt2@gmail.com>
2026-02-08 21:34:48 -0500

    Done

--- Assets/Scripts/AssignmentScripts/ThrowBanana.cs
+++ Assets/Scripts/AssignmentScripts/ThrowBanana.cs
@@ -1,37 +1,33 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using Unity.VisualScripting;
+using UnityEngine;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
 	public class ThrowBanana : ActionTask {
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
-			return null;
-		}
+        public BBParameter<Transform> Banana;
+        public BBParameter<bool> throwBanana;
+        public BBParameter<float> throwForce = 5f;
 
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
-			EndAction(true);
-		}
+        private float timer ;
 
-		//Called once per frame while the action is active.
-		protected override void OnUpdate() {
-			
-		}
 
-		//Called when the task is disabled.
-		protected override void OnStop() {
-			
-		}
 
-		//Called when the task is paused.
-		protected override void OnPause() {
-			
-		}
-	}
+        protected override void OnExecute () {
+            timer = 0f;
+
+        }
+
+
+
+        protected override void OnUpdate() {
+            timer += Time.deltaTime;
+         
+
+        }
+
+    }
 }
