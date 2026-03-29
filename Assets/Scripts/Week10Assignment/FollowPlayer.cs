3bc49d89804d401add8fc32be728f10ed7ed6da5
Aryobt <aryobt2@gmail.com>
2026-03-29 11:44:49 -0400

    Working on it

---
+++ Assets/Scripts/Week10Assignment/FollowPlayer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class FollowPlayer : ActionTask {
+
+		public BBParameter<Transform> PlayerPosition;
+		public float moveSpeed = 3f;
+		public float rotateSpeed = 5f;
+		public float stopDistance = 5f;
+
+		protected override void OnUpdate() {
+			if (PlayerPosition.value == null)
+			{
+				EndAction(false);
+				return;
+			}
+
+            float distance = Vector3.Distance(agent.transform.position, PlayerPosition.value.position);
+
+
+            if (distance < stopDistance)
+			{
+				EndAction(true);
+				return;
+			}
+            Vector3 direction = (PlayerPosition.value.position - agent.transform.position).normalized;
+
+            Vector3 newDirection = Vector3.RotateTowards(agent.transform.forward,direction,rotateSpeed * Time.deltaTime, 0f );
+
+
+			agent.transform.forward = newDirection;
+            agent.transform.position += agent.transform.forward * moveSpeed * Time.deltaTime;
+
+
+        }
+
+
+    }
+}
